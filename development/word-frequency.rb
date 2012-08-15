824aef100d5a6212087ef8ccd39f68cebb243553
Erik Brännström <erik.brannstrom@streambur.se>
2012-08-15 17:11:10 +0200

    Scripts for creating tag lists

--- development/word-frequency.rb
+++ development/word-frequency.rb
@@ -1,10 +1,11 @@
+# coding: UTF-8
 file = ARGV[0]
 
 dict = Hash.new
 f = File.open(file, "r")
 f.each_line { |line|
   line.split.each { |w|
-    w.gsub!(/\W+/, '') # trim non-word characters
+    w.gsub!(/[^\p{L}]/, '') # trim non-word characters
     next if w.length == 0 # skip empty strings
     w.downcase! # all lowercase
     if dict.has_key?(w)
@@ -16,7 +17,6 @@ f.each_line { |line|
 }
 
 # sort the hash by value, and then print it in this sorted order
-dict.sort{|a,b| a[1]<=>b[1]}.each { |elem|
+dict.sort{|a,b| b[1]<=>a[1]}.each { |elem|
   puts "\"#{elem[0]}\" has #{elem[1]} occurrences"
 }
-puts "#{dict.length} words in total."
